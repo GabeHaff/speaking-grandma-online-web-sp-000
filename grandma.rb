683b7a728b971edaaa7190292dde22608eda00c8
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-10-02 15:13:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,7 @@ def speak_to_grandma(greeting)
   if greeting==.upcase 
     puts "NO, NOT SINCE 1938!"
 end 
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
