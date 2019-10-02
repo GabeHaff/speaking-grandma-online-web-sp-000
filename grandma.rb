cae426224a864091be7c606e9b084609b26dcd72
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-10-02 15:16:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(greeting)
   if greeting==greeting.upcase 
     puts "NO, NOT SINCE 1938!"
+    elsif greeting=="I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
 end 
 end
 # Whatever you say to grandma, she should respond with
